386cd3a449fb2af2fba71d471907a269e3affa00
iman-san <iman@syvlabs.com>
2022-03-19 00:25:10 +0800

    edit msg

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Hi Ryan
         </p>
 
         <div className={styles.grid}>
