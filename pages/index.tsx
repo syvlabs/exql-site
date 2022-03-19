256e6542cdfb4d48026358557721ac4fd651dbcf
iman-san <iman@syvlabs.com>
2022-03-19 20:25:43 +0800

    color fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,13 @@ import { motion } from "framer-motion"
 
 const Home: NextPage = () => {
   return <>
-    <div className="bg-gradient-to-t from-emerald-200">
-      <div className="bg-red-500 px-4 text-md md:text-lg text-white py-2 text-center">
+    <div className="bg-gradient-to-t from-main-200">
+      <div className="bg-tmred-dark px-4 text-md md:text-lg text-white py-2 text-center">
         🚫🚀 Unlike everyone else, we have NOT raised any funding, please <a className="underline" href="mailto:exql@googlegroups.com">email us</a> if you want in!
       </div>
       <div className="mx-auto px-4 mt-4 md:mt-10 max-w-7xl select-none">
         <div className="flex items-center">
-          <div className="text-3xl md:text-5xl text-emerald-900 font-medium">ExQL</div>
+          <div className="text-3xl md:text-5xl text-main-900 font-medium">ExQL</div>
           <div className="flex-1"></div>
           <RequestDemo />
         </div>
@@ -21,11 +21,11 @@ const Home: NextPage = () => {
         <div className="mt-6 md:mt-8 text-md md:text-lg px-4 md:px-16 mx-auto items-center text-center max-w-3xl leading-normal">
           Through an Excel-like interface, ExQL enables business analysts to explore, query data, and build SQL data pipelines on top of their data warehouses — without actually writing a line of SQL
         </div>
-        <img className="mt-6 md:mt-16 border-2 border-green-500 rounded-2xl mx-auto items-center shadow-xl" src="/Collaborative.png" />
+        <img className="mt-6 md:mt-16 border-2 border-main-300 rounded-2xl mx-auto items-center shadow-xl" src="/Collaborative.png" />
         <div className="h-4 md:h-16"></div>
       </div>
     </div>
-    <div className="bg-emerald-200 pt-16">
+    <div className="bg-main-200 pt-16">
       <FunctionalFact side="left" />
       <div className="h-16 md:h-32"></div>
       <FunctionalFact side="right" />
@@ -56,8 +56,8 @@ const Home: NextPage = () => {
     <div className="pt-4 pb-8 md:py-16 md:pb-24 bg-gradient-to-b">
       <div className="flex mx-auto px-4 max-w-7xl select-none">
         <div className="mt-8">
-          <div className="text-3xl text-emerald-900 font-medium mb-1">ExQL</div>
-          <div className="text-sm text-emerald-900">2022 ExQL (© coming soon)</div>
+          <div className="text-3xl text-main-900 font-medium mb-1">ExQL</div>
+          <div className="text-sm text-main-900">2022 ExQL (© coming soon)</div>
         </div>
       </div>
     </div>
@@ -65,7 +65,7 @@ const Home: NextPage = () => {
 }
 
 const RequestDemo: FC = () =>
-  <div className="flex w-36 md:w-48 h-10 md:h-12 items-center text-md md:text-lg font-medium rounded-lg select-none text-white bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 cursor-pointer">
+  <div className="flex w-36 md:w-48 h-10 md:h-12 items-center text-md md:text-lg font-medium rounded-lg select-none text-white bg-main-400 hover:bg-main-500 active:bg-main-600 cursor-pointer">
     <span className="mx-auto">Request a demo</span>
   </div>
 
@@ -81,7 +81,7 @@ const FunctionalFact: FC<{ side: "left" | "right" }> = (props) => {
       <img className="md:flex-1 border-2 border-gray-200 rounded-2xl shadow-xl min-w-0 max-w-full self-start" src="/sheet1.png" />}
     <div className={`flex-0 md:w-1/3 text-gray-800 ${props.side === "left" ? "md:mr-16" : "md:ml-16"} mb-4`}>
       <div className="text-4xl mb-4">Explore your big data in an instant ✨</div>
-      <div>Open and explore <span className="font-medium text-emerald-700">terabytes</span> of data from your data warehouse in seconds, through an Excel-like interface</div>
+      <div>Open and explore <span className="font-medium text-main-700">terabytes</span> of data from your data warehouse in seconds, through an Excel-like interface</div>
     </div>
     {props.side === "left" &&
       <img className="md:flex-1 border-2 border-gray-200 rounded-2xl shadow-xl min-w-0 max-w-full self-start" src="/sheet1.png" />}
@@ -90,7 +90,7 @@ const FunctionalFact: FC<{ side: "left" | "right" }> = (props) => {
 
 const NonFunctionalFact: FC<{ title: string, icon: (props: SVGProps<SVGSVGElement>) => JSX.Element }> = (props) => {
   return <div className="flex-1 text-center px-4 pb-12 md:pb-0">
-    <props.icon className="h-24 mx-auto text-amber-600" />
+    <props.icon className="h-24 mx-auto text-tmred-dark" />
     <div className="text-2xl font-medium">{props.title}</div>
     <div className="text-lg mt-4">We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          200: '#a8cdc2',
+          300: '#8bbcad',
+          400: '#6eab99',
+          500: '#58897a',
+          600: '#42675c',
+          700: '#2c443d',
+          800: '#16221f',
+          900: '#0b110f',
+        },
+        tmorange: '#F3A258',
+        tmred: {
+          og: '#EF4631',
+          dark: '#bf3827',
+        }
+      },
+    },
   },
   plugins: [],
 }
