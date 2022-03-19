ee3765af11a36d12b1b1caab4ea7018f1c2989c7
iman-san <iman@syvlabs.com>
2022-03-19 18:56:23 +0800

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
-import { FC } from 'react'
+import { FC, SVGProps } from 'react'
 import { OfficeBuildingIcon, PuzzleIcon, UserGroupIcon } from '@heroicons/react/solid'
 import { motion } from "framer-motion"
 
 const Home: NextPage = () => {
   return <>
     <div className="bg-gradient-to-t from-emerald-200">
-      <div className="bg-red-500 px-4 text-lg text-white py-2 text-center">
+      <div className="bg-red-500 px-4 text-md md:text-lg text-white py-2 text-center">
         🚫🚀 Unlike everyone else, we have NOT raised any funding, please <a className="underline" href="mailto:exql@googlegroups.com">email us</a> if you want in!
       </div>
       <div className="mx-auto px-4 mt-10 max-w-7xl select-none">
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
           <div className="flex-1"></div>
           <RequestDemo />
         </div>
-        <div className="mt-20 mx-auto text-5xl items-center text-center max-w-3xl leading-normal">
+        <div className="mt-20 mx-auto text-4xl md:text-5xl items-center text-center max-w-3xl leading-normal">
           Manipulate your data warehouse with Excel. Bye SQL! 👋
         </div>
         <div className="mt-8 text-lg px-16 mx-auto items-center text-center max-w-3xl leading-normal">
@@ -27,37 +27,24 @@ const Home: NextPage = () => {
     </div>
     <div className="bg-emerald-200 pt-16">
       <FunctionalFact side="left" />
-      <div className="h-32"></div>
+      <div className="h-16 md:h-32"></div>
       <FunctionalFact side="right" />
-      <div className="h-32"></div>
+      <div className="h-16 md:h-32"></div>
       <FunctionalFact side="left" />
-      <div className="h-32"></div>
+      <div className="h-16 md:h-32"></div>
     </div>
     <div className="bg-gradient-to-b from-amber-100 to-white px-4">
-      <div className="pt-36 mx-auto max-w-7xl">
-        <div className="text-5xl text-center mb-24 text-amber-900">Your non-functional requirements hehe</div>
-        <div className="flex mb-48 text-amber-800">
-          <div className="flex-1 text-center px-4">
-            <OfficeBuildingIcon className="h-24 mx-auto text-amber-600" />
-            <div className="text-2xl font-medium">Built for the Enterprise</div>
-            <div className="text-lg mt-4">We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. </div>
-          </div>
-          <div className="flex-1 text-center px-4">
-            <PuzzleIcon className="h-24 mx-auto text-amber-600" />
-            <div className="text-2xl font-medium">Open and Integrated</div>
-            <div className="text-lg mt-4">We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. </div>
-          </div>
-          <div className="flex-1 text-center px-4">
-            <UserGroupIcon className="h-24 mx-auto text-amber-600" />
-            <div className="text-2xl font-medium">Amazing Customer Service</div>
-            <div className="text-lg mt-4">We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. </div>
-          </div>
+      <div className="pt-16 md:pt-36 mx-auto max-w-7xl">
+        <div className="text-5xl text-center mb-16 md:mb-24 text-amber-900">Your non-functional requirements hehe</div>
+        <div className="flex flex-col md:flex-row mb-8 md:mb-48 text-amber-800">
+          <NonFunctionalFact icon={OfficeBuildingIcon} title="Built for the Enterprise" />
+          <NonFunctionalFact icon={PuzzleIcon} title="Open and Integrated" />
+          <NonFunctionalFact icon={UserGroupIcon} title="Amazing Customer Service" />
         </div>
       </div>
-
-      <div className="py-16 mx-auto max-w-7xl select-none rounded-2xl bg-gray-900 shadow-xl">
-        <div className="flex mx-auto max-w-6xl px-8">
-          <div className="flex flex-1 text-4xl text-white items-center">
+      <div className="py-8 md:py-16 mx-auto max-w-7xl select-none rounded-2xl bg-gray-900 shadow-xl">
+        <div className="flex flex-col md:flex-row mx-auto max-w-6xl px-8 items-center">
+          <div className="flex flex-1 text-4xl text-white items-center mb-6 md:mb-0">
             Ready to ExQL?
           </div>
           <div className="flex items-center">
@@ -66,7 +53,7 @@ const Home: NextPage = () => {
         </div>
       </div>
     </div>
-    <div className="pt-16 pb-24 bg-gradient-to-b">
+    <div className="pt-4 pb-8 md:py-16 md:pb-24 bg-gradient-to-b">
       <div className="flex mx-auto px-4 max-w-7xl select-none">
         <div className="mt-8">
           <div className="text-3xl text-emerald-900 font-medium mb-1">ExQL</div>
@@ -87,7 +74,7 @@ const FunctionalFact: FC<{ side: "left" | "right" }> = (props) => {
     className={`flex ${props.side === "left" ? "flex-col" : "flex-col-reverse"} md:flex-row max-w-7xl px-4 md:pb-16 mx-auto items-center`}
     initial={{ opacity: 0, y: -100 }}
     whileInView={{ opacity: 1, y: 0 }}
-    viewport={{ once: true, margin: "-30%" }}
+    viewport={{ once: true, margin: "-50%" }}
     transition={{ duration: 0.4, type: "tween" }}
   >
     {props.side === "right" &&
@@ -99,6 +86,14 @@ const FunctionalFact: FC<{ side: "left" | "right" }> = (props) => {
     {props.side === "left" &&
       <img className="flex-1 border-2 border-gray-200 rounded-2xl shadow-xl min-w-0 max-w-full h-auto" src="/sheet1.png" />}
   </motion.div>
+}
+
+const NonFunctionalFact: FC<{ title: string, icon: (props: SVGProps<SVGSVGElement>) => JSX.Element }> = (props) => {
+  return <div className="flex-1 text-center px-4 pb-12 md:pb-0">
+    <props.icon className="h-24 mx-auto text-amber-600" />
+    <div className="text-2xl font-medium">{props.title}</div>
+    <div className="text-lg mt-4">We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. We will lorem ipsum your dolor. </div>
+  </div>
 }
 
 export default Home
